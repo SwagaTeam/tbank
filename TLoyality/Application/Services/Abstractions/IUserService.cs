89bc6ee97a0352f162bc0dea55cb138c72f3c8f5
Никита <n.belikov@modulbank.ru>
2026-04-30 14:33:10 +0500

    Поменял структуру

--- TLoyality/Application/Services/Abstractions/IUserService.cs
+++ TLoyality/Application/Services/Abstractions/IUserService.cs
@@ -8,5 +8,5 @@ public interface IUserService
     Task<UserResponse?> GetUserById(int userId);
     Task<int?> GetUserIdByPhoneNumber(string phoneNumber);
     Task<ICollection<UserResponse>> GetAllUsers();
-    internal Task<User?> GetUserInternal(int userId);
+    public Task<User?> GetUserInternal(int userId);
 }
